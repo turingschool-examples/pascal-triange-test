c4ee9187809f167d72ec48e65e2043dcc862c902
rrgayhart <rrgayhart@gmail.com>
2017-02-22 09:24:40 -0700

    update test suite

--- pascal-test.js
+++ pascal-test.js
@@ -3,8 +3,18 @@ var expect = require('chai').expect;
 var pascal = require('./pascal.js');
 
 describe('pascal', function(){
-  it('should have the correct output when given 5', function(){
+  it('has the correct output when given 5', function(){
     var solution = [1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1];
     expect(pascal(5)).to.deep.equal(solution);
+  });
+
+  xit('has the correct output when given 4', function(){
+    var solution = [1, 1, 1, 1, 2, 1, 1, 3, 3, 1];
+    expect(pascal(4)).to.deep.equal(solution);
+  });
+
+  xit('has the correct output when given 6', function(){
+    var solution = [1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1, 1, 5, 10, 10, 5, 1];
+    expect(pascal(6)).to.deep.equal(solution);
   });
 });
